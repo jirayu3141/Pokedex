85df410cdb1ae70486bcd00b423a1aece35738c0
Jirayu Sirivorawong <z5125805@student.unsw.edu.au>
2021-05-22 21:50:46 +0700

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Jirayu Sirivorawong on 22/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
